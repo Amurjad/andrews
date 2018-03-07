d6153f75b6fd033a4cf7eabed3b818bd2508b714
Andy Nazarov <andynazarov1996@gmail.com>
2018-03-07 16:24:45 +0200

    Start

---
+++ test.js
@@ -0,0 +1,41 @@
+function plus() {
+    var num1, num2, result;
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+    result = num1 + num2;
+    alert (result);
+    // body...
+}
+function minus() {
+    var num1, num2, result;
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+    result = num1 - num2;
+    alert (result);
+    // body...
+}
+function multiplication() {
+    var num1, num2, result;
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+    result = num1 * num2;
+    alert (result);
+
+    // body...
+}
+function division() {
+    var num1, num2, result;
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+    result = num1 / num2;
+    alert (result);
+    // body...
+}
